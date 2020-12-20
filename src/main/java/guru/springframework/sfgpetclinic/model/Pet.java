1dfefdd033ff49d7623a7cbfacf31afb82023796
LampAnagnostou <lampanagnostou@gmail.com>
2020-12-20 13:48:20 +0100

    Added POJOs. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private Owner owner;
+    private PetType petType;
+    private LocalDate birthDate;
+}
